6ef0727aca3bc50f8d63e6f9fe5bde40ca2fd11b
Yaroslav Kulinich <yaroslavsoldat2@gmail.com>
2023-03-04 08:40:50 +0300

    [INPROGRESS]: In progress

--- common/src/main/java/com/example/clockCustomView/common/presentation/Clock.kt
+++ common/src/main/java/com/example/clockCustomView/common/presentation/Clock.kt
@@ -1,4 +1,23 @@
 package com.example.clockCustomView.common.presentation
 
-class Clock {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class Clock @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val pt = Paint()
+        pt.color = -0x7f000100
+        pt.strokeWidth = 12.0f
+        pt.style = Paint.Style.STROKE
+        canvas.drawRect(0f, 0f, canvas.width.toFloat(), canvas.height.toFloat(), pt)
+    }
 }
